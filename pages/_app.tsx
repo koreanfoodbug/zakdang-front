b971629bb9b5c3b73e266dc7af107ee2c8c32846
frodobigc <frodo@bigc.im>
2023-03-18 01:15:31 +0900

    #

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Normalize  } from 'styled-normalize'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    ChannelIO('boot', {
+      "pluginKey": "314bd9cb-b5a4-47e6-93c4-e627b208e95f" // fill your plugin key
+    });
+  }, []);
   return ( 
     <>
       <Head>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { useEffect } from 'react';
 
 export default function Document() {
+  useEffect(() => {
+    ChannelIO('boot', {
+      "pluginKey": "YOUR_PLUGIN_KEY" // fill your plugin key
+    });
+  }, []);
+  
   return (
     <Html lang="en">
       <Head />
       <body>
+        <script dangerouslySetInnerHTML={{
+          __html: `<script>
+          (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.")}var ch=function(){ch.c(arguments)};ch.q=[];ch.c=function(args){ch.q.push(args)};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x)}}if(document.readyState==="complete"){l()}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l)}})();
+      </script>
+      `
+        }}/>
         <Main />
         <NextScript />
       </body>
